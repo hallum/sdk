c290ace97bade3cb6f5ad8fb6bdc8b18cea33990
hallum <github@hallum.org>
2023-07-26 09:48:09 -0400

    Exposing sort fields for tooltip and legend

--- panel.go
+++ panel.go
@@ -335,9 +335,12 @@ type (
 		Show        *bool    `json:"showLegend,omitempty"`
 		DisplayMode string   `json:"displayMode"`
 		Placement   string   `json:"placement"`
+		SortBy      string   `json:"sortBy,omitempty"`
+		SortDesc    string   `json:"sortDesc,omitempty"`
 	}
 	TimeseriesTooltipOptions struct {
 		Mode string `json:"mode"`
+		Sort string `json:"sort"`
 	}
 	FieldConfigDefaults struct {
 		Unit        string            `json:"unit"`
@@ -710,6 +713,11 @@ func NewTimeseries(title string) *Panel {
 						FixedColor: "green",
 						SeriesBy:   "last",
 					},
+				},
+			},
+			Options: TimeseriesOptions{
+				Tooltip: TimeseriesTooltipOptions{
+					Sort: "none",
 				},
 			},
 		},
